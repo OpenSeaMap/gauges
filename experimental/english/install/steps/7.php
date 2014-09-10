3dd5a60d5ffad9442a7c55279e671f8f5751b928
Matthias Urlichs <matthias@urlichs.de>
2014-09-10 19:54:13 +0200

    Moved "operating" directory to main branch
    the experimental stuff will be resurrected later

--- experimental/english/install/steps/7.php
+++
@@ -1,9 +0,0 @@
-<div>
-<p><b>Installation Complete</b></p>
-<p>
-The installation is now complete<br><br>
-Please remove the folder <b> install </b> from your server<br><br>
-Now the necessary steps for automatic updating of data should be performed,<br>
-please look at B.5 at the manual.<br>
-</p>
-</div>
